5851a9fe323b99aa4b7031c370ce16ed98bea215
Java-12-Aero <seaposeidon31accts@gmail.com>
2022-11-16 13:39:21 -0600

    forgot timer_period, new file

--- bno055_pub.py
+++ bno055_pub.py
@@ -2,23 +2,24 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import board
-import adafruit_bmp3xx
-class BMP388_Publisher(Node):
+import adafruit_bno055
+
+class BNO055_Publisher(Node):
     def __init__(self):
-        super().__init__('BMP388_Publisher')
+        super().__init__('BNO055_Publisher')
         i2c = board.I2C()
-        bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+        bno = adafruit_bno055.BNO055_I2C(i2c)
         timer_period = 0.01
     def timer_callback(self):
         msg = String()
-        msg.data = f'Alt (m): {self.bmp.altitude} Pressure: {self.bmp.pressure}'
+        msg.data = f'linear accel: {self.bno.linear_acceleration} gyro: {self.bno.gyro}'
         self.publisher.publish(msg)
 def main(args=None):
     rclpy.init(args=args)
-    bmp388_pub = BMP388_Publisher()
-    rclpy.spin(bmp388_pub)
+    bno055_pub = BNO055_Publisher()
+    rclpy.spin(bno055_pub)
 
-    bmp388_pub.destroy_node()
+    bno055_pub.destroy_node()
     rclpy.shutdown()
 if __name__ == '__main__':
     main()
